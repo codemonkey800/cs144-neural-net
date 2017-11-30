8964e860cb263d759f6754070f279d0fdc5605e2
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-11-29 22:08:53 -0800

    :wrench: Refactored NeuralNetwork class, implemented query() method

--- src/neuralnet.hpp
+++ src/neuralnet.hpp
@@ -1,34 +1,55 @@
 #pragma once
 #include <vector>
+#include "math.hpp"
 #include "matrix.hpp"
 
 namespace NeuralNetwork {
-    const size_t IMAGE_SIZE = 28;
+    template<size_t InputSize>
+    using InputVector = Matrix::Matrix<double, InputSize, 1>;
 
-    struct TrainingInput {
+    template<size_t CurrentLayerSize, size_t PrevLayerSize>
+    using Weights = Matrix::Matrix<double, CurrentLayerSize, PrevLayerSize>;
+
+    template<size_t InputSize>
+    struct TrainingLabel {
         int label;
-        Matrix::Matrix<double, IMAGE_SIZE, IMAGE_SIZE> image;
+        InputVector<InputSize> input;
     };
 
-    using TrainingSet = std::vector<TrainingInput>;
+    template<size_t InputSize>
+    using TrainingSet = std::vector<TrainingLabel<InputSize>>;
 
-    template<size_t Input, size_t Hidden, size_t Output>
+    template<size_t InputSize, size_t HiddenSize, size_t OutputSize>
     class NeuralNetwork {
     public:
-        NeuralNetwork(const double learningRate) : _learningRate{learningRate} {
+        NeuralNetwork(const double learningRate) :
+            _learningRate{learningRate},
+            _inputWeights{Matrix::randomMatrix<HiddenSize, InputSize>()},
+            _hiddenWeights{Matrix::randomMatrix<OutputSize, HiddenSize>()} {
         }
 
-        int query(const Matrix::ColumnVector<double, Input>& input) const {
+        int query(const InputVector<InputSize>& input) const {
+            auto hiddenOutput = Math::sigmoid(_inputWeights * input);
+            auto output = Math::sigmoid(_hiddenWeights * hiddenOutput);
+
+            // Pick result with highest probability of happening.
+            int result = 0;
+            for (int i = 1; i < OutputSize; ++i) {
+                if (output[i][0] <= output[result][0]) continue;
+                result = i;
+            }
+
+            return result;
         }
 
-        void train(const TrainingSet& trainingSet) {
+        void train(const TrainingSet<InputSize>& trainingSet, const bool verbose = false) {
         }
 
     private:
         double _learningRate;
 
-        Matrix::Matrix<double, Input, Hidden> _inputWeights;
-        Matrix::Matrix<double, Hidden, Output> _hiddenWeights;
+        Weights<HiddenSize, InputSize> _inputWeights;
+        Weights<OutputSize, HiddenSize> _hiddenWeights;
     };
 } // NeuralNetwork
 

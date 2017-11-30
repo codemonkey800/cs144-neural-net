10769d507924aeee90078220ee30d2dade3ceeae
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-11-29 22:07:47 -0800

    :wrench: Added percentage and pixel normalization functions to Math namespace

--- src/math.hpp
+++ src/math.hpp
@@ -3,6 +3,14 @@
 #include "matrix.hpp"
 
 namespace Math {
+    inline double normalizePixel(const int pixel) {
+        return (static_cast<double>(pixel) / 255.0) * 0.99 + 0.01;
+    }
+
+    inline double percentage(const size_t count, const size_t total) {
+        return static_cast<double>(count) / static_cast<double>(total) * 100.0;
+    }
+
     inline double sigmoid(const double x, const bool derivative = false) {
         if (derivative) return sigmoid(x) * (1 - sigmoid(x));
         return 1.0 / (1.0 + std::exp(-x));
